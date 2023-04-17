ede584b560d997f88a3c0e28a5fa3ba004f86566
Ciera DePauw <113853138+cieragrace@users.noreply.github.com>
2023-04-17 11:18:58 -0600

    Swap amd import Link

--- src/components/LoginForm/LoginForm.js
+++ src/components/LoginForm/LoginForm.js
@@ -1,13 +1,23 @@
 import './LoginForm.css'
+import { Link } from 'react-router-dom'
 
 const LoginForm = () => {
   return(
     <form className='form-container'>
       <h1 className='h1'>Please Enter Your Log In Info</h1>
       {/* <p class="login-error-message" id="loginError">Please Enter Valid Login Credentials</p> */}
-      <input class="username" id="userName" type="text" placeholder="username" />
-      <input class="password" id="password" type="text" placeholder="password" />
-      <button class="login-button" id="loginButton">All Set</button>
+      <input 
+        className="username" 
+        id="userName" 
+        type="text" 
+        placeholder="username" 
+        />
+      <input 
+        className="password" 
+        id="password" 
+        type="text" 
+        placeholder="password" />
+      <Link to='/profile/:userID' className="login-button" id="loginButton">All Set</Link>
     </form>
   )
 }
