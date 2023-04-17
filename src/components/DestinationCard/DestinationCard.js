a1fc946dd9db2df311c7542f2cd8e57fa2cdc855
Ciera DePauw <113853138+cieragrace@users.noreply.github.com>
2023-04-17 14:33:50 -0600

    Add destination styling

--- src/components/DestinationCard/DestinationCard.js
+++ src/components/DestinationCard/DestinationCard.js
@@ -1,8 +1,11 @@
 import './DestinationCard.css'
 
-const DestinationCard = () => {
+const DestinationCard = ({destination}) => {
   return(
-    <></>
+    <div className='destination-card-container'>
+      <div className='destination-image'>{`${destination.image}`}</div>
+      <h2 className='destination-name'>{`$destination.destination`}</h2>
+    </div>
   )
 }
 
