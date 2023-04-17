9567f6a9dd2016329aee05fd8b37defa5bb052ae
Ciera DePauw <113853138+cieragrace@users.noreply.github.com>
2023-04-17 14:48:42 -0600

    Update userpage render

--- src/components/UsersPage/UserPage.js
+++ src/components/UsersPage/UserPage.js
@@ -1,8 +1,17 @@
 import './UserPage.css'
+import Header from '../Header/Header'
+import Destinations from '../Destinations/Destinations'
+import Trips from '../Trips/Trips'
 
 const UserPage = () => {
   return(
-    <div className='userpage-container'></div>
+    <div className='userpage-container'>
+      <Header />
+      <section className='places-container'>
+        <Trips />
+        <Destinations />
+      </section>
+    </div>
   )
 }
 
