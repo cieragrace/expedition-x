7ae0ddfca6faec8ba8018c91e326523e1742ecf8
Ciera DePauw <113853138+cieragrace@users.noreply.github.com>
2023-04-17 14:48:00 -0600

    Update form location

--- src/components/HomePage/HomePage.js
+++ src/components/HomePage/HomePage.js
@@ -1,23 +1,26 @@
 import './HomePage.css'
 import LoginForm from '../LoginForm/LoginForm'
 
-const HomePage = ({getAllData}) => {
+const HomePage = ({getAllData, travelers, trips, destinations}) => {
   return(
     <div className='homepage-container'>
-      <LoginForm 
-        getAllData={getAllData}
-        />
       <section className="grid-box one"></section>
-    <section className="grid-box two"></section>
-    <section className="grid-box three"></section>
-    <section className="grid-box four"></section>
-    <section className="grid-box five" id="form">
-      <LoginForm  className='formbox'/>
-    </section>
-    <section className="grid-box six"></section>
-    <section className="grid-box seven"></section>
-    <section className="grid-box eight"></section>
-    <section className="grid-box nine"></section>
+      <section className="grid-box two"></section>
+      <section className="grid-box three"></section>
+      <section className="grid-box four"></section>
+      <section className="grid-box five" id="form">
+        <LoginForm 
+          className='formbox'
+          travelers={travelers}
+          trips={trips}
+          destinations={destinations}
+          getAllData={getAllData}
+          />
+      </section>
+      <section className="grid-box six"></section>
+      <section className="grid-box seven"></section>
+      <section className="grid-box eight"></section>
+      <section className="grid-box nine"></section>
     </div>
   )
 }
