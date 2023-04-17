60bb4238635d285df491bd863cf028b5f3063e2e
Ciera DePauw <113853138+cieragrace@users.noreply.github.com>
2023-04-17 11:19:14 -0600

    get API calls

--- src/components/App/App.js
+++ src/components/App/App.js
@@ -1,7 +1,26 @@
 import HomePage from '../HomePage/HomePage';
 import './App.css';
+import getAPIData from '../../apiCalls';
+import { useState } from 'react'
 
 function App() {
+
+  const [travelers, setTravelers] = useState([])
+  const [trips, setTrips] = useState([])
+  const [destinations, setDestinations] = useState([])
+
+  const getAllData = (travelers, trips, destinations) => {
+    Promise.all([getAPIData('travelers'), getAPIData('trips'), getAPIData('destinations')])
+      .then((data) => {
+        setTravelers(travelers)
+        setTrips(trips)
+        setDestinations(destinations)
+        // addUsername()
+        // loadPage()
+      })
+      .catch(err => console.log("To err is human", err))
+  }
+
   return (
     <div className='app-container'>
       <HomePage />
