6574a4cf90717574f6343a98ce5dd49a4286137e
Ciera DePauw <113853138+cieragrace@users.noreply.github.com>
2023-04-17 10:45:54 -0600

    Add style for LoginForm

--- src/components/LoginForm/LoginForm.js
+++ src/components/LoginForm/LoginForm.js
@@ -2,7 +2,13 @@ import './LoginForm.css'
 
 const LoginForm = () => {
   return(
-    <form className='form-container'></form>
+    <form className='form-container'>
+      <h1 className='h1'>Please Enter Your Log In Info</h1>
+      {/* <p class="login-error-message" id="loginError">Please Enter Valid Login Credentials</p> */}
+      <input class="username" id="userName" type="text" placeholder="username" />
+      <input class="password" id="password" type="text" placeholder="password" />
+      <button class="login-button" id="loginButton">All Set</button>
+    </form>
   )
 }
 
