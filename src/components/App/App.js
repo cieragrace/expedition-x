9378e7e651a8d6c044b077f98d9c8ba481c71cf1
Ciera DePauw <113853138+cieragrace@users.noreply.github.com>
2023-04-17 14:48:54 -0600

    hide switch

--- src/components/App/App.js
+++ src/components/App/App.js
@@ -1,7 +1,9 @@
-import HomePage from '../HomePage/HomePage';
-import './App.css';
-import getAPIData from '../../apiCalls';
-import { useState } from 'react'
+import HomePage from '../HomePage/HomePage'
+import UsersPage from '../UsersPage/UserPage'
+import './App.css'
+import getAPIData from '../../apiCalls'
+import { useState, useEffect } from 'react'
+import { Switch, Route } from 'react-router-dom' 
 
 function App() {
 
@@ -11,20 +13,68 @@ function App() {
 
   const getAllData = (travelers, trips, destinations) => {
     Promise.all([getAPIData('travelers'), getAPIData('trips'), getAPIData('destinations')])
-      .then((data) => {
-        setTravelers(travelers)
-        setTrips(trips)
-        setDestinations(destinations)
-        // addUsername()
-        // loadPage()
-      })
-      .catch(err => console.log("To err is human", err))
+    .then((data) => {
+      setTravelers(data[0])
+      setTrips(data[1])
+      setDestinations(data[2])
+      // addUsername()
+    })
+    .catch(err => console.log("To err is human", err))
+}
+
+  useEffect(() => {
+    getAllData();
+    console.log(destinations.destinations)
+  }, []);
+
+  // const addUsername = () => {
+  //   const updatedTravelers = travelers.travelers.map(traveler => ({
+  //       ...traveler, 
+  //       username: `traveler${traveler.id}` 
+  //   }));
+  //   setTravelers(updatedTravelers);
+  // }
+
+  const getData = (travelerID) => {
+    return travelers.travelers.find(currentUser => currentUser.id === travelerID)
   }
 
+  const findTravelersFlights = () => {
+    return trips.filter(trip => trip.userID === this.travelers.id)
+  }
+
+
+//   <div className='app-container'>
+//   <Switch>
+//     <Route
+//     exact
+//     path='/'
+//     render={() => {
+//       <HomePage 
+//         getAllData={getAllData}
+//         travelers={travelers}
+//         trips={trips}
+//         destinations={destinations}
+//       />
+//     }}
+//     />
+//     <Route
+//     exact
+//     path='/userpage'
+//     render={() => {
+//       <UsersPage 
+//         trips={trips}
+//         destinations={destinations}
+//       />
+//     }}
+//       />
+//   </Switch>
+// </div>
   return (
     <div className='app-container'>
-      <HomePage 
-      getAllData={getAllData}
+      <UsersPage 
+      trips={trips}
+      destinations={destinations}
       />
     </div>
   );
